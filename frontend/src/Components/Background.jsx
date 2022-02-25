5b4cc4cb95f7359d1d24d0039ad2336cfa87f446
PrasanthChowhan <57288401+PrasanthChowhan@users.noreply.github.com>
2022-02-25 09:29:15 +0530

    properly working background

--- frontend/src/Components/Background.jsx
+++ frontend/src/Components/Background.jsx
@@ -3,23 +3,37 @@ import Assets from "../utils/Assets.js";
 import { getImage } from "../utils/getImage.js";
 
 function Background() {
-  const img = null
-  // console.log(Object.keys(Assets.backgrounds));
+  const [img, setImg] = useState(null);
+  // delete from here
+
+  //
+
+  const changebg = (e) => {
+    const location = e.target.id;
+    console.log(img);
+    getImage(location, (image) => {
+      setImg(image);
+    });
+
+    console.log(img);
+    const background = document.getElementById("bg");
+    background.src = img;
+  };
 
   return (
     <>
-      {Object.keys(Assets.backgrounds).map((item) => {
-        {
-          console.log(Assets.backgrounds[item]);
-          
-         
-          
-        }
-
-        <figure>
-          <img src={img} alt="" />
-        </figure>;
-      })}
+      <div key={"01"}>
+        {Object.keys(Assets.backgrounds).map((item, i) => {
+          return (
+            <>
+              <button onClick={changebg} id={Assets.backgrounds[item]} key={i}>
+                {item}
+              </button>
+            </>
+          );
+        })}
+      </div>
+      <img src={img} alt="" id="bg" key={"02"} />
     </>
   );
 }
--- frontend/src/utils/getImage.js
+++ frontend/src/utils/getImage.js
@@ -1,5 +1,31 @@
-export const getImage = (location, set) => {
+// export const getImage = async (location, set) => {
+//   console.log(location);
+//   const sliced = location.split("/");
+//   const log = await import(`../Assets/${sliced[2]}/${sliced[3]}`).then((img) => {
+//     set(img.default);
+//     console.log('inside import')
+    
+//   });
+//   console.log('set')
+//   console.log(log)
+// };
+
+
+//delete from heree
+export const getImage = (location, callback) => {
+  console.log(location);
   const sliced = location.split("/");
-  import(`../Assets/${sliced[2]}/${sliced[3]}`).then((img) => {set = img.default
-  console.log(set)});
+  const log =  import(`../Assets/${sliced[2]}/${sliced[3]}`).then((img) => {
+    callback(img.default)
+    console.log(callback)
+    console.log('inside import')
+    
+  });
+  console.log('set')
+  console.log(log)
 };
+
+
+
+// 
+
